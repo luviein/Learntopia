1d7b67b2fea48c671d41444a52ff07369e40cf9c
Yenleng <yenlengggggg@gmail.com>
2023-09-04 03:27:19 +0800

    initial commit

---
+++ TestProj/testProjSpring/src/main/java/com/example/testProjSpring/TestProjSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.testProjSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjSpringApplication.class, args);
+	}
+
+}
---
+++ TestProj/testProjSpring/src/test/java/com/example/testProjSpring/TestProjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testProjSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
